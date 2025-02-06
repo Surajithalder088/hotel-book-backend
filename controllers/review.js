4e24a180a4834531da0946fc8b8da90db6619625
Surajit <surajithalder088@gmail.com>
2025-02-06 17:02:56 +0530

    review controller added

--- controllers/review.js
+++ controllers/review.js
@@ -2,10 +2,25 @@ import customerModel from "../models/customer.js";
 import hotelModel from "../models/hotel.js";
 import serviceModel from "../models/service.js";
 import receiptModel from "../models/receipt.js";
+import reviewModel from "../models/review.js"
 
 export const createReview=async(req,res)=>{
     try{
-        res.send("review created")
+        const{text,ratings,serviceid,receiptId} =req.body
+        if(!text || !ratings ||! serviceid ||!receiptId){
+           return res.status(400).json({message:'all fields are required'})
+        }
+        const userEmail=req.user.email
+        const user =await customerModel.findOne({email:userEmail})
+        if(!user){
+         return  res.status(404).json({message:"user not found"})
+        }
+        const newReview= await reviewModel.create({text,ratings,service:serviceid,receipt:receiptId,user:user._id})
+        if(!newReview){
+            return  res.status(400).json({message:"review not created"})
+           }
+
+           res.status(201).json({message:"review created",newReview})
 
     }catch(error){
         res.status(500).json({message:'internal server error',error})
@@ -13,7 +28,20 @@ export const createReview=async(req,res)=>{
 }
 export const reviewByService=async(req,res)=>{
     try{
-        res.send("review by service")
+        const {serviceId}=req.params;
+        
+        const service=await serviceModel.findOne({_id:serviceId})
+        if(!service){
+           return res.status(400).json({message:'no service exist'})
+        }
+        
+        
+        const reviews= await reviewModel.find({service:service._id})
+        if(!reviews){
+            return res.status(400).json({message:'no reviews of this service'})
+         }
+
+        res.status(200).json({message:"all reviews ",reviews})
 
     }catch(error){
         res.status(500).json({message:'internal server error',error})
@@ -21,7 +49,20 @@ export const reviewByService=async(req,res)=>{
 }
 export const reviewByReceipt=async(req,res)=>{
     try{
-        res.send("review by receipt")
+        const {receiptId}=req.params;
+        
+        const receipt=await receiptModel.findOne({_id:receiptId})
+        if(!receipt){
+           return res.status(400).json({message:'no service exist'})
+        }
+        
+        
+        const review= await reviewModel.findOne({receipt:receipt._id})
+        if(!review){
+            return res.status(400).json({message:'no reviews of this service'})
+         }
+
+        res.status(200).json({message:"review of this receipt ",review})
 
     }catch(error){
         res.status(500).json({message:'internal server error',error})
