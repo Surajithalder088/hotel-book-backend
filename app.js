e9e594b6bf002a4d00df7ef740e02209cb6ea6be
Surajit <surajithalder088@gmail.com>
2025-03-06 16:16:34 +0530

    m

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 app.use(cors({
-    origin:["https://sito-henna.vercel.app/","http://localhost:3000",],
+    origin:"https://sito-henna.vercel.app",
     credentials:true,
     methods:"GET,POST,PUT,DELETE"
 }))
