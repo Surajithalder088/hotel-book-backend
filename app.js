1f1f257e6caaa09a27e07d74803154b1a6517c1d
Surajit <surajithalder088@gmail.com>
2025-03-06 15:51:31 +0530

    m

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 app.use(cors({
-    origin:["*","http://localhost:3000"],
+    origin:["https://sito-hotels.onrender.com/","http://localhost:3000"],
     credentials:true,
     methods:"GET,POST,PUT,DELETE"
 }))
@@ -32,7 +32,7 @@ app.post('/create-payment-intent',async(req,res)=>{
     const {amount}=req.body
     try {
         const paymentIntent=await stripe.paymentIntents.create({
-            amount:10000,
+            amount:100,// 100 cents == 1 $
             currency:'usd',
             payment_method_types:["card"]
         })
